fdc7c97cd971f1d14e61f0e14d7260a663038549
sho6000 <shounsalaji@gmail.com>
2025-02-27 16:47:24 +0530

    updated code

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from feature import generate_presentation
 
 # Streamlit app title
 st.title("Slide Saver | PowerPoint Presentation Generator")
-st.set_page_config(page_title="Slide Saver | PowerPoint Presentation Generator", layout="wide")
 
 # User input section
-st.header("version 2.0")
+st.header("version 1.0")
 
 # Text input for content with an expanded text area
 lyrics = st.text_area("Enter your lyrics here:", height=250, help="leave gaps after each stanza for the presentaion to appear on each slide")
@@ -14,4 +13,4 @@ lyrics = st.text_area("Enter your lyrics here:", height=250, help="leave gaps af
 # Call the function to generate the presentation
 generate_presentation(lyrics)
 
-# st.markdown("You can find the source code on [GitHub](https://github.com/sho6000/Slide-Saver).")
+st.markdown("You can find the source code on [GitHub](https://github.com/sho6000/Slide-Saver).")
