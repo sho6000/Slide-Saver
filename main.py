41d77f706fb26da9936ebfcbf882510b73330a99
sho6000 <shounsalaji@gmail.com>
2025-02-27 13:20:05 +0530

    modified main.py again

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from feature import generate_presentation
 
 # Streamlit app title
+st.title("Slide Saver | PowerPoint Presentation Generator")
 st.set_page_config(page_title="Slide Saver | PowerPoint Presentation Generator", layout="wide")
 
 # User input section
